d84b90e01f384d1d0ff8b33c5cb096bcd5195344
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2022-10-04 00:09:42 +0300

    Add files via upload

---
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -0,0 +1,13 @@
+package classesWithAttributes;
+
+public class ProductManager {
+
+	public void Add(Product product) {
+		System.out.println(" ürün eklendi.");
+	}
+
+	public void Add2(int id, String name, String description) {
+
+	}
+
+}
